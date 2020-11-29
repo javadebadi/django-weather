d9309ff5aade5bb3df97f7a995ee5283f0821176
javadebadi <javad.ebadi.1990@gmail.com>
2020-11-29 19:21:08 +0330

    Added API to access weather of a city

--- lookup/views.py
+++ lookup/views.py
@@ -1,6 +1,28 @@
+import json
+import requests
 from django.shortcuts import render
 
 app_name="lookup"
-# Create your views here.
-def home(request):
-    return render(request, "lookup/home.html", {})
+
+def getCityWeather(city):
+    # use API to get data about a city's temperature
+    api_request = requests.get("http://api.openweathermap.org/data/2.5/weather?q={}&appid=ed3db698182da566fc01319601b81ccd".format(city))
+
+    try:
+        api = json.loads(api_request.content)
+    except Exception as e:
+        api = {"message":"city does not exist"}
+        
+    try:
+        temp = "{:0.2}".format(api["main"]["temp"] - 273.15)
+    except:
+        pass
+
+    return city, temp, api
+
+def home(request, city=None):
+    city, temp, api = getCityWeather(city="New York")
+    context = {"api": api, "temp": temp, "city": city}
+
+
+    return render(request, "lookup/home.html", context)
